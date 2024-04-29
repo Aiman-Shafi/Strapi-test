e8ab3aefefaee22d149739b17a22ec87d0069dc7
Aiman Shafi <shafiaiman88@gmail.com>
2024-04-29 22:35:46 +0600

    contact form added

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,7 +5,6 @@ export default function Blog() {
   const { error, data, loading } = useFetch(
     `${import.meta.env.VITE_APP_URL}/api/blogs?populate=*`
   );
-
   if (loading) {
     return (
       <div className="text-center">
@@ -28,12 +27,70 @@ export default function Blog() {
         <h2 className="text-3xl font-bold sm:text-4xl text-indigo-700 mb-10">
           Our Insights
         </h2>
+
+        <FormElementsSelectPlainLgHelperText blog={data} />
+
         <div className="grid grid-cols-3 gap-10">
           {data?.map((blog) => (
             <BlogCard blog={blog} key={blog.id} />
           ))}
         </div>
       </section>
+    </>
+  );
+}
+
+// eslint-disable-next-line react/prop-types
+function FormElementsSelectPlainLgHelperText({ blog }) {
+  return (
+    <>
+      {/*<!-- Component: Plain large select with helper text --> */}
+      <div className="relative my-6 md:w-60">
+        <select
+          id="id-08"
+          name="id-08"
+          required
+          className="peer relative h-12 w-full appearance-none border-b border-slate-200 bg-white px-4 text-slate-500 outline-none transition-all autofill:bg-white focus:border-emerald-500 focus-visible:outline-none focus:focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+        >
+          <option value="" disabled selected>
+            Show All
+          </option>
+          {/* {blog.map((post) => (
+            <option value={post.attributes.Category} key={post.id}>
+              {post.attributes.Category}
+            </option>
+          ))} */}
+
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+        </select>
+        <label
+          htmlFor="id-08"
+          className="pointer-events-none absolute top-3 left-2 z-[1] px-2 text-base text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-valid:-top-2 peer-valid:text-xs peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+        >
+          Select an option
+        </label>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="pointer-events-none absolute top-3.5 right-2 h-5 w-5 fill-slate-400 transition-all peer-focus:fill-emerald-500 peer-disabled:cursor-not-allowed"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-labelledby="title-08 description-08"
+          role="graphics-symbol"
+        >
+          <title id="title-08">Arrow Icon</title>
+          <desc id="description-08">Arrow icon of the select list.</desc>
+          <path
+            fillRule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <small className="absolute flex w-full justify-between px-4 py-1 text-xs text-slate-400 transition">
+          <span>Text field with helper text</span>
+        </small>
+      </div>
+      {/*<!-- End Plain large select with helper text --> */}
     </>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,84 @@
+import { useState } from "react";
+import axios from "axios";
+
+import toast, { Toaster } from "react-hot-toast";
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const getContactData = (event) => {
+    event.preventDefault();
+
+    console.log(formData);
+
+    const name = formData.name;
+    const email = formData.email;
+    const phone = formData.phone;
+    const message = formData.message;
+
+    const contactData = {
+      data: {
+        name: name,
+        email: email,
+        phone: phone,
+        message: message,
+      },
+    };
+
+    axios
+      .post("http://localhost:1337/api/contact", contactData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          toast.success("Form Submitted Successfully");
+          setFormData({
+            name: "",
+            email: "",
+            phone: "",
+            message: "",
+          });
+        }
+      })
+      .catch((error) => {
+        // Handle any errors here
+        toast.error("Something went wrong!");
+        console.error("Submission error:", error);
+      });
+  };
+
   return (
     <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          duration: 2000,
+          style: {
+            background: "#363631",
+            color: "#ffffff",
+            fontSize: "22px",
+          },
+        }}
+      />
       <section className="bg-gray-100">
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
@@ -27,16 +105,19 @@ export default function Contact() {
             </div>
 
             <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-              <form action="#" className="space-y-4">
+              <form action="#" className="space-y-4" onSubmit={getContactData}>
                 <div>
                   <label className="sr-only" htmlFor="name">
                     Name
                   </label>
                   <input
+                    name="name"
                     className="w-full rounded-lg border-gray-200 border-2 p-3 text-sm"
                     placeholder="Name"
                     type="text"
                     id="name"
+                    value={formData.name}
+                    onChange={handleChange}
                   />
                 </div>
 
@@ -50,6 +131,9 @@ export default function Contact() {
                       placeholder="Email address"
                       type="email"
                       id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
                     />
                   </div>
 
@@ -62,6 +146,9 @@ export default function Contact() {
                       placeholder="Phone Number"
                       type="tel"
                       id="phone"
+                      name="phone"
+                      value={formData.phone}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -76,6 +163,9 @@ export default function Contact() {
                     placeholder="Message"
                     rows="8"
                     id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
                   ></textarea>
                 </div>
 
